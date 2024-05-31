1f199013e0c78bef7f0e8b4078b56374a80ef271
Brian Akhtar <kngf222@gmail.com>
2024-05-30 23:26:59 -0500

    added next auth lib file

---
+++ libs/next-auth-options.ts
@@ -0,0 +1,13 @@
+// libs/next-auth-options.ts
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
